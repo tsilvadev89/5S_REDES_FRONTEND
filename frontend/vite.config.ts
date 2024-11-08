dcda8ead9d7b205060113901012d4ae65dbdaf1d
tsilvadev89 <165035188+tsilvadev89@users.noreply.github.com>
2024-11-08 12:22:44 -0300

    UpdateFrontProduction

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Separar dependências do React
+          'react-vendor': ['react', 'react-dom'],
+          // Separar MUI em outro chunk
+          'mui-vendor': ['@mui/material', '@mui/icons-material']
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000 // Ajuste do limite de aviso de tamanho do chunk
+  }
+});
