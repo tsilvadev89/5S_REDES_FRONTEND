9e715574f708e5cc7231d6a36f66b37692f757e5
tsilvadev89 <165035188+tsilvadev89@users.noreply.github.com>
2024-11-22 07:27:15 -0300

    UP

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { useThemeContext } from "./theme/ThemeContextProvider";
 
-import { BrowserRouter as Router, Routes } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom'; // Corrigido para usar o Router certo
 import RoutesConfig from "./Routes/RoutesConfig";
 
 import { TemplateProvider, useTemplate } from "./theme/Template";
@@ -13,17 +13,18 @@ function App() {
   const { theme } = useThemeContext();
   const { template } = useTemplate();
 
+ 
+  const basePath = import.meta.env.VITE_BASE_PATH_FRONTEND || "/"; // Use "/" como padrão se não houver variável
+
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <Router>
+      <Router basename={basePath}>
         <ThemeProvider theme={theme}>
           <TemplateProvider template={template}>
-            <Routes />
-            <CssBaseline />
             <RoutesConfig />
+            <CssBaseline />
           </TemplateProvider>
         </ThemeProvider>
-
       </Router>
     </LocalizationProvider>
   );
