5fdd042906cdfc99544e51c24c832e2dc2c24e17
tsilvadev89 <165035188+tsilvadev89@users.noreply.github.com>
2024-11-22 09:24:21 -0300

    UpFront

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
+  // Definir basePath a partir da variável de ambiente ou um valor padrão
   const basePath = process.env.VITE_BASE_PATH_FRONTEND || '/';
+
+  // Definir a porta com base em uma variável de ambiente, ou 80 por padrão
+  const port = Number(process.env.VITE_FRONTEND_PORT) || 80;  // Garantir que seja um número
+
   return {
     base: basePath,
     build: {
@@ -17,7 +22,7 @@ export default defineConfig(({ mode }) => {
     },
     server: {
       host: true,
-      /* port: 80, */
+      port: port, 
     },
   };
 });
