83ecbb0019fc3f48cff6561943dcd684e6791fed
tsilvadev89 <165035188+tsilvadev89@users.noreply.github.com>
2024-11-26 00:20:45 -0300

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
-  // Definir basePath a partir da variável de ambiente ou um valor padrão
   const basePath = process.env.VITE_BASE_PATH_FRONTEND || '/';
-
-  // Definir a porta com base em uma variável de ambiente, ou 80 por padrão
-  const port = Number(process.env.VITE_FRONTEND_PORT) || 80;  // Garantir que seja um número
+  const port = Number(process.env.VITE_FRONTEND_PORT) || 80;
 
   return {
     base: basePath,
@@ -17,11 +14,12 @@ export default defineConfig(() => {
             'mui-vendor': ['@mui/material', '@mui/icons-material'],
           },
           onwarn(warning, warn) {
+            // Ignorar avisos relacionados a "use strict" em módulos antigos
             if (warning.code !== 'MODULE_LEVEL_DIRECTIVE') warn(warning);
           },
         },
-        minify: true, // garantir que a minificação ocorra
-        sourcemap: false, // desabilitar sourcemaps em produçãoa
+        minify: true,
+        sourcemap: false, // Desabilitar sourcemaps em produção
       },
       chunkSizeWarningLimit: 2000,
     },
@@ -36,6 +34,13 @@ export default defineConfig(() => {
     server: {
       host: true,
       port: port,
+      proxy: {
+        '/api': {
+          target: 'http://3.215.171.253:3000',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
     },
   };
 });
